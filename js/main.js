10b70c408a55929aed220fa0b9a5f596360f8d8e
Christopher Barnaby <cjbarnaby@gmail.com>
2015-08-25 06:53:56 +1000

    scroll snap basic

--- js/main.js
+++ js/main.js
@@ -2,24 +2,45 @@ $(document).ready(function() {
 
   skrollr.init();
 
-  // Bones of code to snap to slides.
+  // Bones of code to snap to slides. Works, but performance is bad and needs animation/easing.
 
   // var vH;
 
-  // var getVH = function() {
+  // var getvH = function() {
   //   vH = $(window).height();
   //   return vH;
   // }
 
-  // var topAbout = vH;
-  // var topSkills = vH * 2;
-  // var topProjects = vH * 3;
-  // var topConnect = vH * 4;
-  // var topGoodbye = vH * 5;
+  // $(document).scroll(setInterval(function() {
+
+  //   getvH();
+
+  //   var topAbout = vH;
+  //   var topSkills = vH * 2;
+  //   var topProjects = vH * 3;
+  //   var topConnect = vH * 4;
+  //   var topGoodbye = vH * 5;
+
+  //   if (($(document).scrollTop() > (0.7 * vH)) && $(document).scrollTop() < (1.20 * vH)) {
+  //     $(document).scrollTop(topAbout);
+  //   } else if (($(document).scrollTop() > (1.7 * vH)) && $(document).scrollTop() < (2.20 * vH)) {
+  //     $(document).scrollTop(topSkills);
+  //   } else if (($(document).scrollTop() > (2.7 * vH)) && $(document).scrollTop() < (3.20 * vH)) {
+  //     $(document).scrollTop(topProjects);
+  //   } else if (($(document).scrollTop() > (3.7 * vH)) && $(document).scrollTop() < (4.20 * vH)) {
+  //     $(document).scrollTop(topConnect);
+  //   } else if ($(document).scrollTop() > (4.7 * vH)) {
+  //     $(document).scrollTop(topGoodbye);
+  //   }
+
+  // }, 1000));
+
+
 
   
   // $(document).scroll(function() {
   //     if (($(document).scrollTop() > (0.95 * vH)) && ($(document).scrollTop() < (1 * vH))) {
+  //       console.log("Yes");
   //       $(document).scrollTop(2* vH);
   //     }
   // });
